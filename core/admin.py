bc87342b78b5916c28d2a1b02163a0e7460304f9
Tornesko <konstantinborovenskij@gmail.com>
2022-08-03 23:14:00 +0300

    create ticket and commend models

---
+++ core/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Comment, Ticket
+
+# @admin.site.register(User)
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ["id", "operator", "client", "theme"]
+    list_filter = ["operator"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "ticket"]
