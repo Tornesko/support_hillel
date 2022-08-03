368661832babb22a3a2eb25622b5c472d033536b
Tornesko <konstantinborovenskij@gmail.com>
2022-08-03 14:00:39 +0300

    add authentication to installed apps

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "authentication",
 ]
 
 MIDDLEWARE = [
@@ -98,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "core.User"
+AUTH_USER_MODEL = "authentication.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
