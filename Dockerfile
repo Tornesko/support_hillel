881e4ceb0fd9bf5d2e4eec8c64b4696a8eb477c0
Tornesko <konstantinborovenskij@gmail.com>
2022-09-29 14:41:48 +0300

    dz 25 dockerization

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,6 @@ COPY . .
 RUN pip install pipenv \
     && pipenv install --system --deploy --ignore-pipfile --dev
 
-
-CMD ["python", "manage.py", "runserver", "0.0.0.0:80"]
+CMD sleep 3 \
+    && python manage.py migrate \
+    && python manage.py runserver 0.0.0.0:80
--- config/settings.py
+++ config/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "support",
+        "USER": "support",
+        "PASSWORD": "support",
+        "HOST": "postgres",
+        "PORT": 5432,
     }
 }
 
